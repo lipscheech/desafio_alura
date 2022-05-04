2ee296059e5d9c6e503fd833ca5c347158973cc5
Filipe Cavalcante dos Santos <filipe.santos.estagiario@mma.gov.br>
2022-05-04 11:09:48 -0300

    Melhorando layout e inserindo a funcionalidade de desencriptar

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 var input = document.querySelector("textarea");
-// document.getElementById('containerWithText').style.visibility = "none"
 let textBoxText = document.getElementById('textBoxText')
 let boxText = document.getElementById('boxText')
 let boxTextAfter = document.getElementById('boxAfter')
@@ -11,49 +10,37 @@ console.log(input.value);
 function criptografar() {
     let text = [];
 
-    input.value.split('').forEach(t => {
-        if (t == 'e')
-            text.push('enter')
-        else if (t == 'i')
-            text.push('imes')
-        else if (t == 'a')
-            text.push('ai')
-        else if (t == 'o')
-            text.push('ober')
-        else if (t == 'u')
-            text.push('ufat')
-        else
-            text.push(t)
-    })
+    if (input.value) {
+        input.value.split('').forEach(t => {
+            if (t == 'e')
+                text.push('enter')
+            else if (t == 'i')
+                text.push('imes')
+            else if (t == 'a')
+                text.push('ai')
+            else if (t == 'o')
+                text.push('ober')
+            else if (t == 'u')
+                text.push('ufat')
+            else
+                text.push(t)
+        })
 
-    console.log(text.join(''));
-    // document.getElementById('text').style.visibility = "none"
-    // document.getElementById('containerWithText').style.visibility = "block"
-    boxText.style.setProperty('display', 'none')
-    boxTextAfter.style.setProperty('display', 'block')
-    textBoxText.textContent = text.join('')
+        boxText.style.setProperty('display', 'none')
+        boxTextAfter.style.setProperty('display', 'block')
+        textBoxText.textContent = text.join('')
+    }
 }
 
 function descriptografar() {
-    let text = input.value.replace('ai', 'a')
 
-    text = text.replaceAll('enter', 'e')
-    text = text.replaceAll('imes', 'i')
-    text = text.replaceAll('ober', 'o')
-    text = text.replaceAll('ufat', 'u')
+    if (input.value) {
+        let text = input.value.replace('ai', 'a')
+        text = text.replaceAll('enter', 'e')
+        text = text.replaceAll('imes', 'i')
+        text = text.replaceAll('ober', 'o')
+        text = text.replaceAll('ufat', 'u')
 
-
-    
-
-    // return input.value = text;
-
-    // console.log(text.join(''));
-    // return input.value = text.join('')
+        textBoxText.textContent = text
+    }
 }
-
-// var encrypt = document.querySelector("encrypt");
-// var descrypt = document.querySelector("descrypt");
-
-
-// encrypt.onclick = criptografar;
-// descrypt.onclick = descriptografar;
